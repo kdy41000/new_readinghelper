280e7f49ae87bd48f571c82bf2a0d0e9e4b85991
kdy41000 <kdy41000@naver.com>
2021-03-10 21:51:21 +0900

    first commit

---
+++ ReadingHelper/src/main/java/com/project/readinghelper/ReadingHelperApplication.java
@@ -0,0 +1,13 @@
+package com.project.readinghelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingHelperApplication.class, args);
+	}
+
+}
